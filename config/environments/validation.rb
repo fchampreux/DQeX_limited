3f5f291ab9f45df97c2468f7b3d0211f4e30d8a3
fred <frederic.champreux@bfs.admin.ch>
2022-05-16 14:33:36 +0200

    Complete configuration read

--- config/environments/validation.rb
+++ config/environments/validation.rb
@@ -3,6 +3,11 @@ Sidekiq.configure_server do |config|
   config.redis = { url: Rails.application.credentials.sidekiq[:redis_url] }
   config.on(:startup) do
     schedule_file = "config/sidekiq-cron.yml"
+
+    if File.exist?(schedule_file)
+      Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+    end
+  end
 end
 
 Sidekiq.configure_client do |config|
